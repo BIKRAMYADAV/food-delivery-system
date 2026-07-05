c9358d78a1cca00d528a600bc45a67abadce5cda
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2026-07-05 19:22:44 +0530

    feat: integrated kafka producer into location service

--- delivery-tracker/delivery-service/internal/service/location_service.go
+++ delivery-tracker/delivery-service/internal/service/location_service.go
@@ -1,21 +1,32 @@
 package service
 
 import (
-	"log"
 	"delivery-service/internal/model"
+	"delivery-service/pkg/kafka"
+	"encoding/json"
+	"log"
 )
 
-type LocationService struct{}
+type LocationService struct {
+	producer *kafka.Producer
+}
 
-func NewLocationService() *LocationService {
-	return &LocationService{}
+func NewLocationService(producer *kafka.Producer) *LocationService {
+	return &LocationService{
+		producer: producer,
+	}
 }
 
 func (s *LocationService) ProcessLocation(location model.LocationUpdate) error {
+	data, err := json.Marshal(location)
+	if(err != nil){
+		return err
+	}
 	log.Printf("delivery_partner=%s lat=%f lon=%f",
 		location.DeliveryPartnerID,
 		location.Latitude,
 		location.Longitude,
 	)
-	return nil
+	// return nil
+	return s.producer.Publish(data)
 }
